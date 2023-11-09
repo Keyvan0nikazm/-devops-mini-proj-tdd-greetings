9baaba64c1bc77473fc92f1d5bd8df6e4540d947
Mamadou Barry <mamadou.barry@student.vinci.be>
2023-11-09 14:53:38 +0100

    add: case 1: one name as parameter

--- src/app.test.js
+++ src/app.test.js
@@ -1,6 +1,7 @@
 const greet = module.require("./app"); 
 
-test('should return test', () => {
-    const result = greet("test");
-    expect(result).toEqual("test");
-  });
+test('should return Hello, Jean-Kevin', () => {
+    const result = greet("Jean-Kevin");
+    console.log(result);
+    expect(result).toEqual("Hello, Jean-Kevin");
+});
