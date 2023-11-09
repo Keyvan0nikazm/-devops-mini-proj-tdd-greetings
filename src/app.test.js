ee6b3fd5ff24df84fc59a49f5a808fcd7c7d9a91
steven eklou <steven.eklou@student.vinci.be>
2023-11-09 15:17:41 +0100

    add : case 2 when parameter is null undifined or empty

--- src/app.test.js
+++ src/app.test.js
@@ -5,3 +5,21 @@ test('should return Hello, Jean-Kevin', () => {
     console.log(result);
     expect(result).toEqual("Hello, Jean-Kevin");
 });
+
+test('If the name is undifined should return Hello, my friend', () => {
+  const result = greet(undefined);
+  console.log(result);
+  expect(result).toEqual("Hello, my friend");
+});
+
+test('If the name null should return Hello, my friend', () => {
+  const result = greet(null);
+  console.log(result);
+  expect(result).toEqual("Hello, my friend");
+});
+
+test('If the name is empty should return Hello, my friend', () => {
+  const result = greet("");
+  console.log(result);
+  expect(result).toEqual("Hello, my friend");
+});
