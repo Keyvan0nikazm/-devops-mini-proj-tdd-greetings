3d95b056207b263ef6f7eb6c7168a5225d0cd3b5
Mamadou Barry <mamadou.barry@student.vinci.be>
2023-11-11 21:53:13 +0100

    add: case 7: adding the case where there is a language in the array

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ function greet(name){
     if (Array.isArray(name)){
         let upperCaseName;
         let arrayNamesLowerCase = [];
+        let language;
+        if (name[name.length -1] === "fr" || name[name.length -1] === "nl" || name[name.length -1] === "en"){
+            language = name[name.length -1];
+        }
         if(name.length>=2){
             for (let index = 0; index < name.length; index++) {
                 if(isUpperCase(name[index])){
@@ -12,25 +16,26 @@ function greet(name){
                     upperCaseName = name[index];
                 }
                 else{
-                    arrayNamesLowerCase.push(name[index]);
+                    if (name[index] !== language)
+                        arrayNamesLowerCase.push(name[index]);
                     }
                 }
             }
             if(upperCaseName)
-                return stringConstructor(arrayNamesLowerCase) + ". AND HELLO " + upperCaseName + "!";
+                return stringConstructor(arrayNamesLowerCase) + " AND HELLO " + upperCaseName + "!";
             else{
-                return stringConstructor(name);
+                return stringConstructor(arrayNamesLowerCase, language);
             }
         }
 
     else{
         if (!name) {
-            return "Hello, my friend";
+            return "Hello, my friend.";
         }
         if(isUpperCase(name)){
             return "HELLO, " + name + "!";
         }
-        return "Hello, " + name;
+        return "Hello, " + name + ".";
     }
 };
 
@@ -38,14 +43,27 @@ function isUpperCase(name){
     return name === name.toUpperCase();
 }
 
-function stringConstructor(names){
-    let string = "Hello";
+function stringConstructor(names, language){
+    let greetingWord;
+    let andConnector;
+        if (!language || language === 'en'){
+        greetingWord = "Hello";
+        andConnector = " and ";
+    }
+    else if (language === 'fr'){
+        greetingWord = 'Bonjour';
+        andConnector = " et ";
+    }
+    else if (language === "nl"){
+        greetingWord = "Goeidag";
+        andConnector = " en ";
+    }
+    
     for (let index = 0; index < names.length-1; index++) {
-        string += ", " + names[index];
+        greetingWord += ", " + names[index];
     } 
-    return string + " and " + names[names.length-1];
+    return greetingWord + andConnector + names[names.length-1] + ".";
 }
-
 
 
 module.exports = greet;
--- src/app.test.js
+++ src/app.test.js
@@ -3,25 +3,25 @@ const greet = module.require("./app");
 test('should return Hello, Jean-Kevin', () => {
     const result = greet("Jean-Kevin");
     console.log(result);
-    expect(result).toEqual("Hello, Jean-Kevin");
+    expect(result).toEqual("Hello, Jean-Kevin.");
 });
 
 test('If the name is undifined should return Hello, my friend', () => {
   const result = greet(undefined);
   console.log(result);
-  expect(result).toEqual("Hello, my friend");
+  expect(result).toEqual("Hello, my friend.");
 });
 
 test('If the name null should return Hello, my friend', () => {
   const result = greet(null);
   console.log(result);
-  expect(result).toEqual("Hello, my friend");
+  expect(result).toEqual("Hello, my friend.");
 });
 
 test('If the name is empty should return Hello, my friend', () => {
   const result = greet("");
   console.log(result);
-  expect(result).toEqual("Hello, my friend");
+  expect(result).toEqual("Hello, my friend.");
 });
 
 test('If the name is write in UpperCase should return HELLO, KENOBI!', () => {
@@ -33,13 +33,13 @@ test('If the name is write in UpperCase should return HELLO, KENOBI!', () => {
 test('If the name is an array should return Hello, Kratos and Thanathos', () => {
     const result = greet(["Kratos", "Thanathos"]);
     console.log(result);
-    expect(result).toEqual("Hello, Kratos and Thanathos");
+    expect(result).toEqual("Hello, Kratos and Thanathos.");
   });
 
   test('If the name is an array with more than two names should return Hello, Kratos, Thanathos and Hypnos', () => {
     const result = greet(["Kratos", "Thanathos", "Hypnos"]);
     console.log(result);
-    expect(result).toEqual("Hello, Kratos, Thanathos and Hypnos");
+    expect(result).toEqual("Hello, Kratos, Thanathos and Hypnos.");
   });
 
   test('If the name is an array with more than two names and one word in upperCase should return Hello, Thanathos and Hypnos. AND HELLO KRATOS!', () => {
@@ -52,4 +52,22 @@ test('If the name is an array should return Hello, Kratos and Thanathos', () => 
     const result = greet(["KRATOS", "Thanathos","Hypnos","KENOBI"]);
     console.log(result);
     expect(result).toEqual("Impossible because more than 1 uppercase name");
+  });
+
+  test('If the name is an array with more than two names and with a language should return Impossible because more than 1 uppercase name', () => {
+    const result = greet(["Kratos", "Thanathos", "fr"]);
+    console.log(result);
+    expect(result).toEqual("Bonjour, Kratos et Thanathos.");
+  });
+
+  test('If the name is an array with more than two names and with a language should return Impossible because more than 1 uppercase name', () => {
+    const result = greet(["Kratos", "Thanathos", "nl"]);
+    console.log(result);
+    expect(result).toEqual("Goeidag, Kratos en Thanathos.");
+  });
+
+  test('If the name is an array with more than two names and with a language should return Impossible because more than 1 uppercase name', () => {
+    const result = greet(["Kratos", "Thanathos", "en"]);
+    console.log(result);
+    expect(result).toEqual("Hello, Kratos and Thanathos.");
   });
